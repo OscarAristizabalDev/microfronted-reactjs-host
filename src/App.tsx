39f2a8f6c38bf013a893e164bbad1839fe417685
OscarAristizabalDev <oscar.aristi.jaimes@gmail.com>
2023-06-20 09:04:31 -0500

    Applied styles in app host - microfrontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
-
 import { Navbar } from "navbar/Navbar"; // se importa el componente navbar, microfronted
-
 import { Counter } from "counterReact/CounterReact"; // se importa el componente counter, microfronted
+
+import "./index.css";
 
 const App = () => (
   <>
     <Navbar />
-    <h2>Host App</h2>
-
-    <Counter />
+    <div className="App-container">
+      <h2>Counter MF</h2>
+      <Counter />
+    </div>
   </>
 );
 ReactDOM.createRoot(document.getElementById("app") as HTMLElement).render(<App />);
